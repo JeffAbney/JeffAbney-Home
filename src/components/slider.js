502b566a25754c55b407701b2e214923ac2b4ada
JeffAbney <jeffmabney@gmail.com>
2018-11-29 16:12:26 -0600

    Add specific classNames to each slider

--- src/components/slider.js
+++ src/components/slider.js
@@ -4,8 +4,7 @@ const sliderFunctions =
   {
     1: (el) => {
       return (
-        <div className={`slider-grid ${el.id % 2 === 0 ? 'right' : 'left'}`}>
-          <div className="slider-bar" id="bar-1"></div>
+<div className={`slider-grid slider-1 ${el.id % 2 === 0 ? 'right' : 'left'}`}>          <div className="slider-bar" id="bar-1"></div>
           <div className="slider-bar" id="bar-2"></div>
           <div className="slider-bar" id="bar-3"></div>
           <div className="slider-bar" id="bar-4"></div>
@@ -15,8 +14,7 @@ const sliderFunctions =
     },
     2: (el) => {
         return (
-          <div className={`slider-grid ${el.id % 2 === 0 ? 'right' : 'left'}`}>
-            <div className="slider-bar" id="bar-1"></div>
+<div className={`slider-grid slider-2 ${el.id % 2 === 0 ? 'right' : 'left'}`}>            <div className="slider-bar" id="bar-1"></div>
             <div className="slider-bar" id="bar-2"></div>
             <div className="slider-bar" id="bar-3"></div>
             <div className="slider-bar" id="bar-4"></div>
@@ -29,8 +27,7 @@ const sliderFunctions =
       },
     3: (el) => {
         return (
-          <div className={`slider-grid ${el.id % 2 === 0 ? 'right' : 'left'}`}>
-            <div className="slider-bar" id="bar-1"></div>
+<div className={`slider-grid slider-3 ${el.id % 2 === 0 ? 'right' : 'left'}`}>            <div className="slider-bar" id="bar-1"></div>
             <div className="slider-bar" id="bar-2"></div>
             <div className="slider-bar" id="bar-3"></div>
             <div className="slider-bar" id="bar-4"></div>
@@ -40,8 +37,7 @@ const sliderFunctions =
 
     4: (el) => {
         return (
-          <div className={`slider-grid ${el.id % 2 === 0 ? 'right' : 'left'}`}>
-            <div className="slider-bar" id="bar-1"></div>
+<div className={`slider-grid slider-4 ${el.id % 2 === 0 ? 'right' : 'left'}`}>            <div className="slider-bar" id="bar-1"></div>
             <div className="slider-bar" id="bar-2"></div>
             <div className="slider-bar" id="bar-3"></div>
             <div className="slider-bar" id="bar-4"></div>
@@ -58,7 +54,7 @@ const sliderFunctions =
 
     5: (el) => {
         return (
-          <div className={`slider-grid ${el.id % 2 === 0 ? 'right' : 'left'}`}>
+          <div className={`slider-grid slider-5 ${el.id % 2 === 0 ? 'right' : 'left'}`}>
             <div className="slider-bar" id="bar-1"></div>
             <div className="slider-bar" id="bar-2"></div>
             <div className="slider-bar" id="bar-3"></div>
