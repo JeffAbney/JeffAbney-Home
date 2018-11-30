f2e85eab351c9fa4d93f10a4cf00c9051b60aec5
JeffAbney <jeffmabney@gmail.com>
2018-11-30 11:01:21 -0600

    Add 3 animations

--- src/components/slider.js
+++ src/components/slider.js
@@ -5,34 +5,34 @@ const sliderFunctions =
     1: (el) => {
       return (
 <div className={`slider-box slider-1 ${el.id % 2 === 0 ? 'right' : 'left'}`}>
-          <div className="slider-bar bar-1"></div>
-          <div className="slider-bar bar-2"></div>
-          <div className="slider-bar bar-3"></div>
-          <div className="slider-bar bar-4"></div>
-          <div className="slider-bar bar-5"></div>
+          <div className="slider-bar bar-1 horizontal-slider"></div>
+          <div className="slider-bar bar-2 horizontal-slider"></div>
+          <div className="slider-bar bar-3 horizontal-slider"></div>
+          <div className="slider-bar bar-4 horizontal-slider"></div>
+          <div className="slider-bar bar-5 horizontal-slider"></div>
         </div>
         )
     },
     2: (el) => {
         return (
 <div className={`slider-box slider-2 ${el.id % 2 === 0 ? 'right' : 'left'}`}>
-            <div className="slider-bar bar-1"></div>
-            <div className="slider-bar bar-2"></div>
-            <div className="slider-bar bar-3"></div>
-            <div className="slider-bar bar-4"></div>
-            <div className="slider-bar bar-5"></div>
-            <div className="slider-bar bar-6"></div>
-            <div className="slider-bar bar-7"></div>
+            <div className="slider-bar bar-1 vertical-slider"></div>
+            <div className="slider-bar bar-2 vertical-slider"></div>
+            <div className="slider-bar bar-3 vertical-slider"></div>
+            <div className="slider-bar bar-4 vertical-slider"></div>
+            <div className="slider-bar bar-5 vertical-slider"></div>
+            <div className="slider-bar bar-6 vertical-slider"></div>
+            <div className="slider-bar bar-7 vertical-slider"></div>
           </div>
           )
       },
     3: (el) => {
         return (
 <div className={`slider-box slider-3 ${el.id % 2 === 0 ? 'right' : 'left'}`}>
-            <div className="slider-bar bar-1"></div>
-            <div className="slider-bar bar-2"></div>
-            <div className="slider-bar bar-3"></div>
-            <div className="slider-bar bar-4"></div>
+            <div className="slider-bar bar-1 vertical-slider"></div>
+            <div className="slider-bar bar-2 vertical-slider"></div>
+            <div className="slider-bar bar-3 vertical-slider"></div>
+            <div className="slider-bar bar-4 vertical-slider"></div>
           </div>
           )
       },
