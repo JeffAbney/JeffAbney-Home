580006ed894dba2b891da5db19b001da5af32a56
JeffAbney <jeffmabney@gmail.com>
2018-11-30 10:21:03 -0600

    Add initial animation setup.

--- src/components/slider.js
+++ src/components/slider.js
@@ -4,63 +4,66 @@ const sliderFunctions =
   {
     1: (el) => {
       return (
-<div className={`slider-grid slider-1 ${el.id % 2 === 0 ? 'right' : 'left'}`}>          <div className="slider-bar" id="bar-1"></div>
-          <div className="slider-bar" id="bar-2"></div>
-          <div className="slider-bar" id="bar-3"></div>
-          <div className="slider-bar" id="bar-4"></div>
-          <div className="slider-bar" id="bar-5"></div>
+<div className={`slider-box slider-1 ${el.id % 2 === 0 ? 'right' : 'left'}`}>
+          <div className="slider-bar bar-1"></div>
+          <div className="slider-bar bar-2"></div>
+          <div className="slider-bar bar-3"></div>
+          <div className="slider-bar bar-4"></div>
+          <div className="slider-bar bar-5"></div>
         </div>
         )
     },
     2: (el) => {
         return (
-<div className={`slider-grid slider-2 ${el.id % 2 === 0 ? 'right' : 'left'}`}>            <div className="slider-bar" id="bar-1"></div>
-            <div className="slider-bar" id="bar-2"></div>
-            <div className="slider-bar" id="bar-3"></div>
-            <div className="slider-bar" id="bar-4"></div>
-            <div className="slider-bar" id="bar-5"></div>
-            <div className="slider-bar" id="bar-6"></div>
-            <div className="slider-bar" id="bar-7"></div>
-            <div className="slider-bar" id="bar-8"></div>
+<div className={`slider-box slider-2 ${el.id % 2 === 0 ? 'right' : 'left'}`}>
+            <div className="slider-bar bar-1"></div>
+            <div className="slider-bar bar-2"></div>
+            <div className="slider-bar bar-3"></div>
+            <div className="slider-bar bar-4"></div>
+            <div className="slider-bar bar-5"></div>
+            <div className="slider-bar bar-6"></div>
+            <div className="slider-bar bar-7"></div>
           </div>
           )
       },
     3: (el) => {
         return (
-<div className={`slider-grid slider-3 ${el.id % 2 === 0 ? 'right' : 'left'}`}>            <div className="slider-bar" id="bar-1"></div>
-            <div className="slider-bar" id="bar-2"></div>
-            <div className="slider-bar" id="bar-3"></div>
-            <div className="slider-bar" id="bar-4"></div>
+<div className={`slider-box slider-3 ${el.id % 2 === 0 ? 'right' : 'left'}`}>
+            <div className="slider-bar bar-1"></div>
+            <div className="slider-bar bar-2"></div>
+            <div className="slider-bar bar-3"></div>
+            <div className="slider-bar bar-4"></div>
           </div>
           )
       },
 
     4: (el) => {
         return (
-<div className={`slider-grid slider-4 ${el.id % 2 === 0 ? 'right' : 'left'}`}>            <div className="slider-bar" id="bar-1"></div>
-            <div className="slider-bar" id="bar-2"></div>
-            <div className="slider-bar" id="bar-3"></div>
-            <div className="slider-bar" id="bar-4"></div>
-            <div className="slider-bar" id="bar-5"></div>
-            <div className="slider-bar" id="bar-6"></div>
-            <div className="slider-bar" id="bar-7"></div>
-            <div className="slider-bar" id="bar-8"></div>
-            <div className="slider-bar" id="bar-9"></div>
-            <div className="slider-bar" id="bar-10"></div>
-            <div className="slider-bar" id="bar-11"></div>
+<div className={`slider-box slider-4 ${el.id % 2 === 0 ? 'right' : 'left'}`}>
+            <div className="slider-bar bar-1"></div>
+            <div className="slider-bar bar-2"></div>
+            <div className="slider-bar bar-3"></div>
+            <div className="slider-bar bar-4"></div>
+            <div className="slider-bar bar-5"></div>
+            <div className="slider-bar bar-6"></div>
+            <div className="slider-bar bar-7"></div>
+            <div className="slider-bar bar-8"></div>
+            <div className="slider-bar bar-9"></div>
+            <div className="slider-bar bar-10"></div>
+            <div className="slider-bar bar-11"></div>
           </div>
           )
       },
 
     5: (el) => {
         return (
-          <div className={`slider-grid slider-5 ${el.id % 2 === 0 ? 'right' : 'left'}`}>
-            <div className="slider-bar" id="bar-1"></div>
-            <div className="slider-bar" id="bar-2"></div>
-            <div className="slider-bar" id="bar-3"></div>
-            <div className="slider-bar" id="bar-4"></div>
-            <div className="slider-bar" id="bar-5"></div>
-            <div className="slider-bar" id="bar-6"></div>
+          <div className={`slider-box slider-5 ${el.id % 2 === 0 ? 'right' : 'left'}`}>
+            <div className="slider-bar bar-1"></div>
+            <div className="slider-bar bar-2"></div>
+            <div className="slider-bar bar-3"></div>
+            <div className="slider-bar bar-4"></div>
+            <div className="slider-bar bar-5"></div>
+            <div className="slider-bar bar-6"></div>
           </div>
           )
       },
