628727f7b8b6ee1606de48129df042727076ac41
JeffAbney <jeffmabney@gmail.com>
2018-12-11 12:57:11 -0600

    Add headhsot to about section

--- src/components/about.js
+++ src/components/about.js
@@ -3,6 +3,14 @@ import headshot from '/home/jeff/git_workspace/JeffAbney/src/static/images/Jeff-
 
 export default () => (
   <div className="about" id="about">
-  <p>About</p>
+    <div className="text-container about-text-container">
+      <h3>I'm Jeff Abney.</h3>
+      <p>Go getting, problem solving, self-taught and always learning. Bringing a unique perspective to Web Development</p>
+      <p>Born and raised near Chicago, I've lived and traveled in a variety of places.</p>
+      <p>Looking for remote work opportunities as well as freelance work.</p>
+    </div>
+    <div className="image-container about-image-container">
+      <img className="headshot" src={headshot} alt="Image of Jeff Abney"/>
+    </div>
   </div>
   )
