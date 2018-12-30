91a3ec7ff8faebe6a3e4b449865bde3534ba8c45
JeffAbney <jeffmabney@gmail.com>
2018-12-30 11:07:35 -0600

    Resize and center bio image and text.

--- src/components/about.js
+++ src/components/about.js
@@ -4,11 +4,13 @@ import headshot from '/home/jeff/git_workspace/JeffAbney/src/static/images/Jeff-
 export default () => (
   <div className="about" id="about">
     <div className="bio">
-      <div className="text-container about-text-container">
-        <h3>I'm Jeff Abney.</h3>
-        <p>Go getting, problem solving, self-taught and always learning. Bringing a unique perspective to Web Development</p>
-        <p>Born and raised near Chicago, I've lived and traveled in a variety of places.</p>
-        <p>Looking for remote work opportunities and freelance work.</p>
+      <div className="centering">
+        <div className="text-container about-text-container">
+          <h3>I'm Jeff Abney.</h3>
+          <p>Go getting, problem solving, self-taught and always learning. Bringing a unique perspective to Web Development</p>
+          <p>Born and raised near Chicago, I've lived and traveled in a variety of places.</p>
+          <p>Looking for remote work opportunities and freelance work.</p>
+        </div>
       </div>
       <div className="image-container about-image-container">
         <img className="headshot" src={headshot} alt="Image of Jeff Abney"/>
