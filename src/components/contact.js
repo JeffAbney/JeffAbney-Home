25be6db2bc4dc6f1c19bc9f796483510af684b38
JeffAbney <jeffmabney@gmail.com>
2019-01-04 20:23:33 -0600

    Make email in contact a link

--- src/components/contact.js
+++ src/components/contact.js
@@ -5,7 +5,7 @@ export default () => (
   <div className="contact" id="contact">
     <div className="contact-text">
       <p>I'm available for work and I would love to hear about your project.</p>
-      <p>Send me an email at JeffMAbney@gmail.com</p>
+      <p>Send me an email at <a href="mailto:jeffmabney@gmail.com">jeffmabney@gmail.com</a></p>
       <p>Or contact me via social media.</p>
       <img src={arrow} alt="arrow point to social media icons"></img>
     </div>
