85c8bd3ec5b92fc01bd0236c71daa373ee3237c6
JeffAbney <jeffmabney@gmail.com>
2019-01-08 21:45:56 -0600

    Trim quotes from design quotes.

--- src/components/projects/random_quote/randomQuoteMachine.js
+++ src/components/projects/random_quote/randomQuoteMachine.js
@@ -23,7 +23,7 @@ class RandomQuoteMachine extends Component {
       quote: "Your quote is loading",
       author: "Random Quote Machine",
       btnDisabled: true,
-      ready: false 
+      ready: false
     };
     this.handleClick = this.handleClick.bind(this);
     this.APIFetch = this.APIFetch.bind(this);
@@ -31,19 +31,19 @@ class RandomQuoteMachine extends Component {
   }
 
   APIFetch() {
-      fetch(APIURL)
-        .then(response => response.json())
-        .then(data => this.setState({
+    let regEx = /&|#/gi;
+    fetch(APIURL)
+      .then(response => response.json())
+      .then(data => this.setState({
 
-          quoteCount: 0,
-          cache: data,
-          quote: data[0].content,
-          author: data[0].title,
-          btnDisabled: false,
-          ready: true
-        }))
+        quoteCount: 0,
+        cache: data.filter((post) => !regEx.test(post.content)),
+        quote: data[0].content,
+        author: data[0].title,
+        btnDisabled: false,
+        ready: true
+      }))
   }
-
 
   reset() {
     this.setState({
@@ -52,97 +52,99 @@ class RandomQuoteMachine extends Component {
     this.APIFetch();
   }
 
-  handleClick(){
-      this.state.quoteCount === 39 ? this.reset() 
+  handleClick() {
+    this.state.quoteCount === this.state.cache.length - 1 ?
+      this.reset()
       :
-        this.setState({
-          colorCount: Math.floor((Math.random() * 4) + 1),
-          quoteCount: this.state.quoteCount+1,
-          quote: this.state.cache[this.state.quoteCount+1].content,
-          author: this.state.cache[this.state.quoteCount+1].title, 
-         });
+      this.setState({
+        colorCount: Math.floor((Math.random() * 4) + 1),
+        quoteCount: this.state.quoteCount + 1,
+        quote: this.state.cache[this.state.quoteCount + 1].content,
+        author: this.state.cache[this.state.quoteCount + 1].title
+      });
+      console.log(this.state.cache)
   }
 
 
-//Start API call****************************************
+  //Start API call****************************************
 
   componentDidMount() {
     this.APIFetch();
   }
-//End API call*******************************************
+  //End API call*******************************************
 
-//Start render HTML ****************************************************
+  //Start render HTML ****************************************************
   render() {
-    const {colorCount} = this.state;
+    const { colorCount } = this.state;
 
-    return (  
-      <div className= {`page color-${colorCount}`}>
+    return (
+      <div className={`page color-${colorCount}`}>
         <div className="logo-container">
-          <img src={logo} alt="Jeff Abney logo" className="logo"/>
+          <img src={logo} alt="Jeff Abney logo" className="logo" />
         </div>
-    <div
-      className={"grid-container color-"+this.state.colorCount}
-      id="quote-box"
-    >
-      <p className="quote-mark quote-mark-start">“</p>
-      <div className="text-container">
-        {
-          this.state.ready ? 
-            <p
-              className={"quote-text color-"+this.state.colorCount}
-              id ="text">{this.state.quote.slice(3, -5)}</p>
-          :
-            <div
-              className="lds-ellipsis"
-            >
-              <div></div><div></div><div></div><div></div>
-            </div>
-        }
-        
-        
-      </div>
-      <p className="quote-mark quote-mark-stop">”</p>
-      <div className="author-container">
-        <p className={"auto-text color-"+this.state.colorCount} id="author">
-        {this.state.author}
-        </p>
-      </div>
-      <div className="twitter-link-container">
-        <div className="twitter-icon">
-          <a
-            className="twitter-share-button"
-            id="tweet-quote" 
-            href={tweetUrl+this.state.quote+" -"+this.state.author} 
-            target ="_blank"
-          >
-            <i className={"fab fa-twitter color-"+this.state.colorCount}></i>
-          </a> 
-        </div>
-      </div>
-      <button
-        disabled={this.state.btnDisabled}
-        className={"new-quote-btn color-"+this.state.colorCount}
-        id="new-quote"
-        onClick = {this.handleClick}>New Quote</button>
-    </div>
-    <footer className="credit">
-      <p
-        className={"color-"+this.state.colorCount}
-      >
-        Designed by&nbsp;
-        <a
-          href="http://daniela-trujillo.com/"
-        >Daniela Trujillo 
-        </a>
-        &nbsp;and Developed by&nbsp;
-        <a
-          href="mailto: jeffmabney@gmail.com"
+        <div
+          className={"grid-container color-" + this.state.colorCount}
+          id="quote-box"
         >
-          Jeff Abney
+          <p className="quote-mark quote-mark-start">“</p>
+          <div className="text-container">
+            {
+              this.state.ready ?
+                <p
+                  className={"quote-text color-" + this.state.colorCount}
+                  id="text">{this.state.quote.slice(3,-5).replace("&#8217;", "'")}</p>
+                :
+                <div
+                  className="lds-ellipsis"
+                >
+                  <div></div><div></div><div></div><div></div>
+                </div>
+            }
+
+
+          </div>
+          <p className="quote-mark quote-mark-stop">”</p>
+          <div className="author-container">
+            <p className={"auto-text color-" + this.state.colorCount} id="author">
+              {this.state.author}
+            </p>
+          </div>
+          <div className="twitter-link-container">
+            <div className="twitter-icon">
+              <a
+                className="twitter-share-button"
+                id="tweet-quote"
+                href={tweetUrl + this.state.quote + " -" + this.state.author}
+                target="_blank"
+              >
+                <i className={"fab fa-twitter color-" + this.state.colorCount}></i>
+              </a>
+            </div>
+          </div>
+          <button
+            disabled={this.state.btnDisabled}
+            className={"new-quote-btn color-" + this.state.colorCount}
+            id="new-quote"
+            onClick={this.handleClick}>New Quote</button>
+        </div>
+        <footer className="credit">
+          <p
+            className={"color-" + this.state.colorCount}
+          >
+            Designed by&nbsp;
+        <a
+              href="http://daniela-trujillo.com/"
+            >Daniela Trujillo
         </a>
-      </p>
-    </footer>
-    </div>  
+            &nbsp;and Developed by&nbsp;
+        <a
+              href="mailto: jeffmabney@gmail.com"
+            >
+              Jeff Abney
+        </a>
+          </p>
+        </footer>
+      </div>
     );
   }
 }
