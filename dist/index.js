841fa7b15dcfecae4268a09f4629d0e1fadcefdb
JeffAbney <jeffmabney@gmail.com>
2019-01-30 11:09:42 -0600

    Format code

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 import './myStyles.scss';
 
@@ -23,77 +23,77 @@ class App extends React.Component {
 
   componentDidMount() {
     import(/* webpackChunkName: 'Navbar' */ './components/nav.js').then(Navbar => {
-      this.setState({ Navbar: Navbar.default})
+      this.setState({ Navbar: Navbar.default })
     });
     import(/* webpackChunkName: 'Hero' */ './components/hero.js').then(Hero => {
-      this.setState({ Hero: Hero.default})
+      this.setState({ Hero: Hero.default })
     });
     import(/* webpackChunkName: 'Projects' */ './components/projects.js').then(Projects => {
-      this.setState({ Projects: Projects.default})
+      this.setState({ Projects: Projects.default })
     });
     import(/* webpackChunkName: 'Footer' */ './components/footer.js').then(Footer => {
-      this.setState({ Footer: Footer.default})
+      this.setState({ Footer: Footer.default })
     });
     import(/* webpackChunkName: 'About' */ './components/about.js').then(About => {
-      this.setState({ About: About.default})
+      this.setState({ About: About.default })
     });
     import(/* webpackChunkName: 'Contact' */ './components/contact.js').then(Contact => {
-      this.setState({ Contact: Contact.default})
+      this.setState({ Contact: Contact.default })
     });
     import(/* webpackChunkName: 'Pomodoro' */ './components/pomoApp.js').then(Pomodoro => {
-      this.setState({ Pomodoro: Pomodoro.default})
+      this.setState({ Pomodoro: Pomodoro.default })
     });
     import(/* webpackChunkName: 'DrumMachine' */ './components/drumMachine.js').then(DrumMachine => {
-      this.setState({ DrumMachine: DrumMachine.default})
+      this.setState({ DrumMachine: DrumMachine.default })
     });
     import(/* webpackChunkName: 'RandomQuoteMachine' */ './components/randomQuoteMachine.js').then(RandomQuoteMachine => {
-      this.setState({ RandomQuoteMachine: RandomQuoteMachine.default})
+      this.setState({ RandomQuoteMachine: RandomQuoteMachine.default })
     });
     import(/* webpackChunkName: 'Calculator' */ './components/calculator.js').then(Calculator => {
-      this.setState({ Calculator: Calculator.default})
+      this.setState({ Calculator: Calculator.default })
     });
   }
-  
+
 
   render() {
     const { Navbar, Hero, Projects, About, Contact, Footer, Pomodoro, RandomQuoteMachine, Calculator } = this.state;
 
     return (
       <Router>
-        <Route exact={true} path="/" render={() =>(
+        <Route exact={true} path="/" render={() => (
           <div className="app">
-            {Navbar ? <Navbar/> : <p>Loading...</p>}
-            {Hero ? <Hero/> : <p>Loading...</p>}
-            {About ? <About/> : <p>Loading...</p>}
-            {Projects ? <Projects/> : <p>Loading...</p>}
-            {Contact ? <Contact/> : <p>Loading...</p>}
-            {Footer ? <Footer/> : <p>Loading...</p>}
+            {Navbar ? <Navbar /> : <p>Loading...</p>}
+            {Hero ? <Hero /> : <p>Loading...</p>}
+            {About ? <About /> : <p>Loading...</p>}
+            {Projects ? <Projects /> : <p>Loading...</p>}
+            {Contact ? <Contact /> : <p>Loading...</p>}
+            {Footer ? <Footer /> : <p>Loading...</p>}
           </div>
-        )}/>
-        
-        <Route exact={true} path='/pomodoro'render={() =>(
+        )} />
+
+        <Route exact={true} path='/pomodoro' render={() => (
           <div className="app pomodoro">
-          <Pomodoro/>
+            <Pomodoro />
           </div>
-        )}/>
+        )} />
 
-        <Route exact={true} path='/drumMachine'render={() =>(
+        <Route exact={true} path='/drumMachine' render={() => (
           <div className="app drum-machine">
-          <DrumMachine/>
+            <DrumMachine />
           </div>
-        )}/>
+        )} />
 
-        <Route exact={true} path='/randomQuote'render={() =>(
+        <Route exact={true} path='/randomQuote' render={() => (
           <div className="app quote-machine">
-          <RandomQuoteMachine/>
+            <RandomQuoteMachine />
           </div>
-        )}/>
+        )} />
 
-        <Route exact={true} path='/calculator'render={() =>(
+        <Route exact={true} path='/calculator' render={() => (
           <div className="app calculator-app">
-          <Calculator/>
+            <Calculator />
           </div>
-        )}/>
+        )} />
       </Router>
     );
   };
