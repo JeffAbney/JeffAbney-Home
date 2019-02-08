9761670e17773eb0b8df9602cdc8f1949e6ac667
JeffAbney <jeffmabney@gmail.com>
2019-02-07 21:00:56 -0600

    Change button layout

--- src/components/hero.js
+++ src/components/hero.js
@@ -2,15 +2,17 @@ import React from 'react';
 
 export default () => (
   <div className="hero" id="home">
-    <h1>&lt; Developer &gt;</h1>
+    <h1>&lt; Web Developer /&gt;</h1>
     <p>I solve problems and create solutions 
       that help people and companies reach their goals. I specialize
       in developing Single Page Apps with HTML5, CSS, and Javascript with React.
     </p>
-    <div className="hero-btn-container">
-
-      <a href="JeffAbneyResume.pdf" target="_blank">
-        Hire me
+    <div className="btn-container">
+    <a className="button" href="#contact">
+        Get in touch
+        </a>
+      <a className="button" href="JeffAbneyResume.pdf" target="_blank">
+        My Resume
         </a>
     </div>
   </div>
