138d75638f58be921f7960c19842e04ef9765685
JeffAbney <jeffmabney@gmail.com>
2019-02-14 16:58:29 -0600

    Add Linked in to contact

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,6 +3,7 @@ import logo from "/home/jeff/git_workspace/JeffAbney/src/static/images/JEFF_ICON
 import instagram from "/home/jeff/git_workspace/JeffAbney/src/static/images/instagram-logo.png";
 import twitter from "/home/jeff/git_workspace/JeffAbney/src/static/images/twitter.png";
 import github from "/home/jeff/git_workspace/JeffAbney/src/static/images/github-logo.png";
+import linkedin from "/home/jeff/git_workspace/JeffAbney/src/static/images/linkedin.png"
 
 export default () => (
   <div className="footer">
@@ -42,7 +43,17 @@ export default () => (
           className="social-icon github"
         />
       </a>
-      
+      <a
+        href="https://www.linkedin.com/in/jeff-abney-056b05172/"
+        target="_blank"
+      >
+        <img 
+          alt="LinkedIn logo" 
+          src={linkedin}
+          className="social-icon linked"
+        />
+      </a>
+
     </div>
     <div className="credit">
       Developed by <a href="https://jeffabney.com" target="_blank">Jeff Abney</a> &amp; Designed by <a href="https://daniela-trujillo.com" target="_blank">Daniela Trujillo</a>
